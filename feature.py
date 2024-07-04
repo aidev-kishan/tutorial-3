2c1360b71c901636175cc3146b802d5c0e92f6fb
aidev.kishan <kishan.ai.dev@gmail.com>
2024-07-04 09:54:10 +0530

    feature-2 Version 3

--- feature.py
+++ feature.py
@@ -1,1 +1,7 @@
 print("features, version -1 ")
+number_cubes = input("Enter the number you want to cube : -")
+number_cubes = float(number_cubes)
+def cube(numbers):
+    cube = number_cubes*number_cubes*number_cubes
+    print(cube)
+cube(number_cubes)
