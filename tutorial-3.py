cc732345763f9434f47d317aa94be285ebc757c5
aidev.kishan <kishan.ai.dev@gmail.com>
2024-07-04 09:30:14 +0530

    Version 3

--- tutorial-3.py
+++ tutorial-3.py
@@ -1,8 +1,18 @@
 import math
-name = input("Be a King : - ").lower()
-def add_a_king(name):
-    print(f"King {name}!" )
-add_a_king(name)
+import sys
+want = input("Be a King or Queen type yes : - ").lower()
+if want == "yes":
+    genders = input("Are you Load or lady : - load for type 'K' or lady for 'Q'").lower()
+    name = input("What is your name? :- ").lower()
+else:
+    sys.exit()
+def add_a_king(name, genders):
+    if want == "yes":
+        if genders == "k":
+            print(f"King {name}!" )
+        else:
+            print(f"Queen {name}")
+add_a_king(name, genders)
 
 number = input("enter the number want to Square: - ")
 number = float(number)
