affe8a1e9edba52b79aae61b5eed7af68eb71a29
aidev.kishan <kishan.ai.dev@gmail.com>
2024-07-04 09:16:51 +0530

    Version 2

--- tutorial-3.py
+++ tutorial-3.py
@@ -1,4 +1,12 @@
+import math
 name = input("Be a King : - ").lower()
 def add_a_king(name):
     print(f"King {name}!" )
 add_a_king(name)
+
+number = input("enter the number want to Square: - ")
+number = float(number)
+def calculate_square(number):
+    square = number*number
+    print(f"Square {square})!")
+calculate_square(number)
